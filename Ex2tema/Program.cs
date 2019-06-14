835fd42586ee1c936516b5c05902837b241dcfe7
Simona Bolca <simona.bolca@yahoo.com>
2019-06-14 05:14:17 +0300

    commit

---
+++ Ex2tema/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2tema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scrieti un program care accepta de la utilizar doua numere, si afiseaza toate numerele intre ele.
+            //Daca primul numar e mai mic decat al doilea, numerele afisate vor fi in ordine crescatoare.
+            //Daca primul numar e mai mare ca l doilea, numerele afisate for fi in ordine descrescatoare.
+            Console.WriteLine("Introduceti un numar");
+            int primulNumar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti inca un numar");
+            int alDoileaNumar = int.Parse(Console.ReadLine());
+
+             
+                if (primulNumar < alDoileaNumar)
+                {
+                for (int i =primulNumar+1; i < alDoileaNumar; i++)
+                {
+                    Console.WriteLine(i+" ");
+                }
+                    
+                }
+
+
+            if (primulNumar > alDoileaNumar)
+            {
+
+                for (int k = primulNumar - 1; k > alDoileaNumar; k--)
+
+                {
+                    Console.WriteLine(k + " ");
+                }
+
+            }
+           
+                
+
+                
+            
+            Console.ReadKey();
+        } 
+        
+
+    }
+}
