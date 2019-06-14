5317e32e3bc029cc67dd41a6869100f11429e2d0
Simona Bolca <simona.bolca@yahoo.com>
2019-06-14 05:02:04 +0300

    commit proj

---
+++ nrPrim/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nrPrim
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Afisati la consola numerele prime pana la 100.
+            Console.WriteLine("Primele 100 de numere prime sunt:");
+
+
+            
+            for (int i = 2; i < 100; i++)
+            {
+                if (i == 2 || i == 3 || i == 5)
+                {
+                    Console.Write(i + "  ");
+                }
+                if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.Write(i + "  ");
+                }
+
+            }
+           
+            Console.ReadLine();
+            
+                
+                
+            
+                    
+            
+            
+            }
+
+
+        
+
+
+
+        
+
+            
+
+
+        
+            
+        
+               
+
+
+        }
+} 
+
+     
+                
+            
+            
+
+        
+
+    
+
