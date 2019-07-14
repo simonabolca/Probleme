455c0ae9283052db87aa1a630b3daf368cfe7b62
Simona Bolca <simona.bolca@yahoo.com>
2019-07-14 17:19:33 +0300

    commit

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salut");
+
+            Console.WriteLine("care este prenumele tau?");
+            string prenume = Console.ReadLine();
+
+            Console.WriteLine("Care este numele tau?");
+            string nume = Console.ReadLine();
+
+            Console.WriteLine("Unde te-ai nascut?");
+            string oras = Console.ReadLine();
+            Afiseaza( prenume, nume, oras);
+
+            ReverseString(prenume);
+            ReverseString(nume);
+            ReverseString(oras);
+            Console.ReadKey();
+
+        }
+        static string  ReverseString(string intrare)
+        {
+            char[] iesire = intrare.ToArray();
+            Array.Reverse(iesire);
+
+            string result = " ";
+            foreach(char item in iesire)
+            {
+                result += item;
+                
+            }
+            Console.WriteLine(result);
+                return result;
+        }
+        static void Afiseaza( string prenume, string nume, string oras)
+        {
+            Console.WriteLine($" Buna {prenume} {nume},nascuta in {oras} ");
+
+        }
+            
+        }
+    }
+
---
+++ siruri/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex4Tema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Intoroduceti un sir de numere");
+
+            int sir = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '),arrTemp=>int.Parse(arrTemp));
+
+            int t;
+            for (int i = 0; i < arr.Length-2; i++)
+            {
+                for (int j = 0; j < arr.Length-2; j++)
+                {
+                    if(arr[j] > arr[j + 1])
+                    {
+                        t = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = t;
+                    }
+                }
+                
+            }
+            foreach(int nr in arr)
+            {
+                Console.WriteLine(nr + " ");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
